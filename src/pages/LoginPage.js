cc45d2e5c9aed0f644229936099394c6501857ff
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-01 14:52:40 +0100

    [ieo119] Add LoginPage component

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+
+function LoginPage() {
+    return (
+        <div className='LoginPage' align='center'>
+            <h2>Stand-in for login page.</h2>
+        </div>
+    );
+}
+
+export default LoginPage;
