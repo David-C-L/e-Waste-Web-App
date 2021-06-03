63a1f365d0f864df84de531f0cd521fb91ba6fce
dcl19 <dcl19@ic.ac.uk>
2021-06-03 15:31:04 +0100

    [dcl19][ds1619] added form to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,47 @@
 import './Home.css';
 import logo from '../images/recycle-logo.png';
+import React from 'react';
 
-function Home() {
-    return (
-        <div className="Home">
-            <header className="Home-header">
-            <img src={logo} className="Home-logo" alt="logo" />
-            <p>
-                An app to do with sustainability.
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        alert('Post Submitted');
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="Home">
+                <header className="Home-header">
+                    <img src={logo} className="Home-logo" alt="logo" />
+                    <p>
+                        An app to do with sustainability.
             </p>
-            <a
-                className="Home-link"
-                href="https://gitlab.doc.ic.ac.uk/g205001021/drp21"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                create a post
+                    <a
+                        className="Home-link"
+                        href="https://gitlab.doc.ic.ac.uk/g205001021/drp21"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        create a post
             </a>
-            </header>
-        </div>
-    );
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Post:
+                    <input type="text" />
+                        </label>
+                        <input type="submit" value="Post" />
+                    </form>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default Home;
