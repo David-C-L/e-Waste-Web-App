760f45ae0bd3c7bac899d406f926738d354e7ba3
dcl19 <dcl19@ic.ac.uk>
2021-06-03 17:00:14 +0100

    [dcl19][ds1619][ieo119] added category selection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import './Home.css';
 import logo from '../images/recycle-logo.png';
 import React from 'react';
+import Select from 'react-select';
+import MultiSelect from './MultiSelect'
+
 
 class Home extends React.Component {
+
+    ops = [
+        {value: 'recycle', label: 'Recycle'},
+        {value: 'reuse', label: 'Reuse'},
+        {value: 'reduce', label: 'Reduce'}
+    ];
 
     constructor(props) {
         super(props);
@@ -16,28 +25,34 @@ class Home extends React.Component {
     }
 
     render() {
+        const styles = {
+            option: provided => ({
+              ...provided,
+              color: 'black'
+            }),
+            control: provided => ({
+              ...provided,
+              color: 'black'
+            }),
+            singleValue: (provided) => ({
+              ...provided,
+              color: 'black'
+            })
+        }
         return (
             <div className="Home">
                 <header className="Home-header">
                     <img src={logo} className="Home-logo" alt="logo" />
                     <p>
                         An app to do with sustainability.
-            </p>
-                    <a
-                        className="Home-link"
-                        href="https://gitlab.doc.ic.ac.uk/g205001021/drp21"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        create a post
-            </a>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                            Post:
-                    <input type="text" />
-                        </label>
-                        <input type="submit" value="Post" />
-                    </form>
+                    </p>
+                    <div>
+                        <MultiSelect options={this.ops} styles={styles}/>
+                        <form onSubmit={this.handleSubmit}>
+                            <input type="text"/>
+                            <input type="submit" value="Post" />
+                        </form>
+                    </div>
                 </header>
             </div>
         );
---
+++ src/pages/MultiSelect.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+
+const animatedComps = makeAnimated();
+
+export default function MultiSelect(props) {
+  return (
+    <Select
+      closeMenuOnSelect={false}
+      components={animatedComps}
+      isMulti
+      options={props.options}
+      styles={props.styles}
+    />
+  );
+}
