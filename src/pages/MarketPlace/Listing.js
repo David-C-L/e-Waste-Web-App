1e01fed7b46e86bd7e24c96d5c849941f4c9489d
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-09 22:39:29 +0100

    [ieo119] Display data from endpoint on listings page

--- src/pages/MarketPlace/Listing.js
+++ src/pages/MarketPlace/Listing.js
@@ -1,10 +1,19 @@
-import ListingDetails from './ListingDetails';
 import './Listing.css';
+import defaultListing from '../../images/laptop.jpg';
+import { Link } from 'react-router-dom'
 
-function Listing() {
+
+function Listing(props) {
+    console.log("listing: " + props.listing)
     return (
         <div className='Listing'>
-            <ListingDetails />
+            <img src={defaultListing} className='DefaultListingPicture' />
+            <h3 className="Title">{props.listing.title}</h3>
+            <p className="Description"> {props.listing.description} </p>
+            <div id='bottom'>
+                <p className="TimeRemaining"> {props.listing.timeRemaining} </p>
+                <Link className='MessageOwner' to='/'>Message the owner</Link>
+            </div>
         </div>
     );
 }
--- src/pages/MarketPlace/ListingRow.js
+++ src/pages/MarketPlace/ListingRow.js
@@ -1,12 +1,27 @@
 import Listing from './Listing';
 import './ListingRow.css';
 
-function ListingRow() {
+function ListingRow(props) {
+    console.log("Listings array: " + props.listings.length)
     return (
         <div className='Row'>
-            <Listing />
-            <Listing />
-            <Listing />
+            <>
+            { 
+            props.listings.length > 0
+                ? <Listing listing={props.listings[0]}/>
+                : <></> 
+            }
+            {
+            props.listings.length > 1
+                ? <Listing listing={props.listings[1]}/>
+                : <></> 
+            }
+            {
+            props.listings.length > 2
+                ? <Listing listing={props.listings[2]}/>
+                : <></> 
+            }
+            </>
         </div>
     );
 }
