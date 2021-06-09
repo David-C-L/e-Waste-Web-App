90709bf06bdb6c6ed73965975226b0037399c7dc
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-09 22:38:55 +0100

    [ieo119] Make GET request to listings endpoint and store result in state

--- src/pages/MarketPlace/Listings.js
+++ src/pages/MarketPlace/Listings.js
@@ -1,13 +1,35 @@
+import { useState, useEffect } from 'react';
 import ListingRow from './ListingRow';
+import axios from 'axios';
+
+function splitArray(array, groupSize) {
+    const numGroups = Math.ceil(array.length / groupSize);
+    return new Array(numGroups)
+        .fill('')
+        .map((_, i) => array.slice(i * groupSize, (i + 1) * groupSize));
+}
 
 function Listings() {
+
+    const [listings, setListings] = useState([]);
+
     //TODO: GET request to fetch listings and display
+    useEffect(() => {
+        // GET request using axios inside useEffect React hook
+        axios.get('https://drp21-backend.herokuapp.com/api/v1/listings')
+            .then(response => setListings(response.data));
+    }, [listings]);
+
+    console.log(splitArray(listings, 3));
+
     return (
-        <>
-            <ListingRow />
-            <ListingRow />
-            <ListingRow />
-        </>
+        <div className="Listings">
+        { listings === null || listings === undefined
+            ? <p> No Listings </p>
+            : splitArray(listings, 3).map(listingGroup => 
+                <ListingRow listings={listingGroup}/>)
+        }
+        </div>
     );
 }
 
