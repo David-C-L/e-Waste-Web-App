6b10ed5e0205c21e7521fe225feb7c807142f882
Dilan Sheth <ds1619@ic.ac.uk>
2021-06-10 13:35:44 +0100

    [dcl19][ds1619] Moved comment box to bottom right of post

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -24,16 +24,19 @@ function Post(props) {
                     <UserDetails email={props.authorEmail} />
                     <PostDetails date={props.date} time={props.time} />
                 </div>
+                <div className="Center-section">
+                    <h6 className="Title">{props.title}</h6>
+                    <p className="Post-body"> {props.text} </p>
+                </div>
                 <div className="Right-section">
                     <div className="Category">
                         <p className="Category-text">{props.category}</p>
                     </div>
-                    <h6 className="Title">{props.title}</h6>
-                    <p className="Post-body"> {props.text} </p>
                     <button className="Comment-button"
-                        onClick={() => setIsCollapsed(!isCollapsed)}>
-                        {isCollapsed ? 'Show' : 'Hide'} Comments
+                            onClick={() => setIsCollapsed(!isCollapsed)}>
+                            {isCollapsed ? 'Show' : 'Hide'} Comments
                     </button>
+
                 </div>
 
             </div>
