8d2ac34fa714da6e6d277daa50bd60d3a6326f98
Izer Onadim <izer.onadim19@imperial.ac.uk>
2021-06-15 08:48:05 +0100

    [ieo119] Update snapshot test

--- src/pages/tests/Home.test.js
+++ src/pages/tests/Home.test.js
@@ -1,11 +1,10 @@
-import NavBar from '../NavBar';
-import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
+import AppRouter from '../../AppRouter';
 
 
 describe('NavBar component', () => {
     it('matches the snapshot', () => {
-        const tree = renderer.create(<BrowserRouter><NavBar /></BrowserRouter>).toJSON()
+        const tree = renderer.create(<AppRouter/>).toJSON()
 
         expect(tree).toMatchSnapshot()
     })
